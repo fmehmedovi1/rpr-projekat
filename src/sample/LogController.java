49f952339cc544018dfc9ec3b8b218ea37eb81ca
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-12 16:19:52 +0100

    Dodane funkcionalnsti dugmadi u LogController

--- src/sample/LogController.java
+++ src/sample/LogController.java
@@ -2,16 +2,36 @@ package sample;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LogController {
-
+    private UserModel model;
     public TextField fldUsername;
     public PasswordField fldPassword;
 
     public LogController(UserModel model) {
+        this.model = model;
+    }
+
+
+    public void loginAction(ActionEvent actionEvent){
+        if (fldUsername.getText().equals("") || fldPassword.getText().equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("The username or password you’ve entered doesn’t match any account.");
+            alert.setContentText("Sign up for an account.");
+            alert.showAndWait();
+        }
+
 
     }
-    
+
+
+    public void cancelAction(ActionEvent actionEvent){
+        Platform.exit();
+    }
+
+    public void addUserAction(ActionEvent actionEvent){}
 }

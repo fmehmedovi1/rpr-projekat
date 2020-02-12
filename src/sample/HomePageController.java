a6a25bffd370a015426566c4cffe50f7b56123a8
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-12 23:09:33 +0100

    kreirani atributi i metode za HomePageController

--- src/sample/HomePageController.java
+++ src/sample/HomePageController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -15,12 +16,15 @@ import java.io.IOException;
 
 public class HomePageController {
     private UserModel model;
-    public TextField fldUsername;
-    public PasswordField fldPassword;
+    public Label lblName, lblNameWH, lblAddress, lblMyProducts, lblAbout, lblContact;
+
 
     public HomePageController(UserModel model) {
         this.model = model;
     }
 
-    
+    public void actionPrint(ActionEvent actionEvent){}
+
+    public void actionOpenProducts(MouseEvent mouseEvent){}
+
 }

140e1caa16bc75f16a2c0c92770613dac1579b23
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-12 02:22:13 +0100

    Kreiran model za klasu User, te dodati geteri i seteri

---
+++ src/sample/UserModel.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableMap;
+
+public class UserModel {
+
+    private ObservableMap<String, User> users = FXCollections.observableHashMap();
+
+    public ObservableMap<String, User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ObservableMap<String, User> users) {
+        this.users = users;
+    }
+
+    
+}

7739845c0e5a563a8f4115c43724a686bbc2994e
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-13 02:51:54 +0100

    dodan atribut za cuvanje Producta-a, sa get-erima i set-erima

--- src/sample/ProductModel.java
+++ src/sample/ProductModel.java
@@ -1,5 +1,18 @@
 package sample;
 
-public class ProductModel {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
+public class ProductModel {
+    private ObservableList<Product> products = FXCollections.observableArrayList();
+
+    public ProductModel() {}
+
+    public ObservableList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ObservableList<Product> products) {
+        this.products = products;
+    }
 }

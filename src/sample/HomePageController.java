fbe4f56d6735e762d9b4f1c5037b04c6988f4656
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-13 15:48:02 +0100

    ažurirano praćenje broja Product-a na Homepage

--- src/sample/HomePageController.java
+++ src/sample/HomePageController.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,6 +10,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -17,13 +19,19 @@ import java.io.IOException;
 public class HomePageController {
     private UserModel model;
     private ProductModel productModel;
-    public Label lblName, lblNameWH, lblAddress, lblMyProducts, lblAbout, lblContact;
+    public Label lblName, lblNameWH, lblAddress, lblNumber;
 
 
     public HomePageController(UserModel model) {
         this.model = model;
         productModel = new ProductModel();
         productModel.napuni();
+    }
+
+    @FXML
+    public void initialize() {
+        lblName.setText("Faris");
+        lblNumber.setText(String.valueOf(productModel.getProducts().size()));
     }
 
     public void actionPrint(ActionEvent actionEvent){}
@@ -37,6 +45,10 @@ public class HomePageController {
         stage.setTitle("My Products");
         stage.setScene(new Scene(root, 350, 450));
         stage.show();
+
+        stage.setOnHiding(windowEvent -> {
+            lblNumber.setText(String.valueOf(productModel.getProducts().size()));
+        });
     }
 
 

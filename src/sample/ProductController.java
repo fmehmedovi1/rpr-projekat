77c1a541df8e00cc5a8e9f2f5bf659cf592f06f9
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-13 03:39:44 +0100

    dodati atributi i konstruktor u ProductController

--- src/sample/ProductController.java
+++ src/sample/ProductController.java
@@ -1,7 +1,18 @@
 package sample;
 
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
 public class ProductController {
 
+    public ListView<Product> listView;
+    public TextField fldName, fldPrice;
+    public Slider sliderAmount;
+    private ProductModel model;
 
-    
+    public ProductController(ProductModel productModel){}
+
+
+
 }

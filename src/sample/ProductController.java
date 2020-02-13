256d0ff3979569c09175648dd654dc26fb6644df
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-13 11:43:41 +0100

    implementirana funkcionalnost tableView

--- src/sample/ProductController.java
+++ src/sample/ProductController.java
@@ -1,15 +1,18 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ProductController {
 
-    public ListView<Product> listView;
+    public TableView<Product> tableView;
     public TextField fldName, fldPrice;
     public Slider sliderAmount;
+    public TableColumn colName, colPrice, colAmount;
     private ProductModel model;
 
     public ProductController(ProductModel productModel){
@@ -18,32 +21,9 @@ public class ProductController {
 
     @FXML
     public void initialize() {
-
-        listView.setItems(model.getProducts());
-        listView.getSelectionModel().selectedItemProperty().addListener((obs, oldProduct, newProduct) -> {
-            model.setCurrentProduct(newProduct);
-            model.getCurrentProduct().setAmount((int)sliderAmount.getValue());
-            listView.refresh();
-        });
-
-        model.currentProductProperty().addListener((obs, oldProduct, newProduct) -> {
-            if (oldProduct != null){
-                fldName.textProperty().unbindBidirectional(oldProduct.nameProperty() );
-                fldPrice.textProperty().unbindBidirectional(oldProduct.nameProperty() );
-                sliderAmount.valueProperty().unbindBidirectional(oldProduct.amountProperty());
-            }
-
-            if (newProduct == null){
-                fldName.setText("");
-                fldPrice.setText("");
-            }
-            else {
-                fldName.textProperty().bindBidirectional(newProduct.nameProperty());
-                fldPrice.textProperty().bindBidirectional(newProduct.priceProperty());
-                sliderAmount.valueProperty().bindBidirectional(newProduct.amountProperty());
-            }
-
-        });
-
+        tableView.setItems(model.getProducts());
+        colName.setCellValueFactory(new PropertyValueFactory("Name"));
+        colPrice.setCellValueFactory(new PropertyValueFactory("Price"));
+        colAmount.setCellValueFactory(new PropertyValueFactory("Amount"));
     }
 }

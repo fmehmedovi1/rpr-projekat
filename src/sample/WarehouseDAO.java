545856d4d1a6ce6074b4dcbfbbce208e99c48531
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-14 02:53:39 +0100

    implementirane metode za dodavanje User-a i Product-a

--- src/sample/WarehouseDAO.java
+++ src/sample/WarehouseDAO.java
@@ -9,8 +9,8 @@ public class WarehouseDAO {
 
     private static WarehouseDAO instance;
     private static Connection conn;
-    private PreparedStatement getProducts, getWarehouse, deleteProduct, updateProduct,
-            addUser, addProduct;
+    private PreparedStatement getProductsStm, getWarehouseStm, deleteProductStm, updateProductStm,
+            addUserStm, addProductStm;
 
     private WarehouseDAO(){
         try {
@@ -21,22 +21,20 @@ public class WarehouseDAO {
 
 
         try {
-            getProducts = conn.prepareStatement("SELECT * FROM products p, warehouse_products wp, warehouses w " +
+            getProductsStm = conn.prepareStatement("SELECT * FROM products p, warehouse_products wp, warehouses w " +
                     "WHERE warehouse_id = wp_warehouse_id AND wp.product_id = p.product_id AND w.warehouse.name = ?");
-            getWarehouse = conn.prepareStatement("SELECT * FROM warehouses w, users u " +
+            getWarehouseStm = conn.prepareStatement("SELECT * FROM warehouses w, users u " +
                     "WHERE w.responsible_preson_id = u.user_id AND u.username = ?");
 
-            deleteProduct = conn.prepareStatement("DELETE FROM products WHERE id = ?");
-            updateProduct = conn.prepareStatement("UPDATE product SET name=?, price=?, quantity=? WHERE id=?");
+            deleteProductStm = conn.prepareStatement("DELETE FROM products WHERE id = ?");
+            updateProductStm = conn.prepareStatement("UPDATE product SET name=?, price=?, quantity=? WHERE id=?");
 
-            addUser = conn.prepareStatement("INSERT INTO product VALUES(?,?,?,?,?,?)");
-            addProduct = conn.prepareStatement("INSERT INTO product VALUES(?,?,?,?)");
+            addUserStm = conn.prepareStatement("INSERT INTO product VALUES(?,?,?,?,?,?)");
+            addProductStm = conn.prepareStatement("INSERT INTO product VALUES(?,?,?,?)");
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
     }
 
 
@@ -51,7 +49,7 @@ public class WarehouseDAO {
         instance = null;
     }
 
-    public void close() {
+    private void close() {
         try {
             conn.close();
         } catch (SQLException e) {
@@ -59,7 +57,33 @@ public class WarehouseDAO {
         }
     }
 
+    public void addUser(User user){
+        try {
+            addUserStm.setInt(1, user.getId());
+            addUserStm.setString(2,user.getFirstName());
+            addUserStm.setString(3,user.getLastName());
+            addUserStm.setString(4,user.getUsername());
+            addUserStm.setString(5,user.getEmail());
+            addUserStm.setString(6,user.getPassword());
+            addUserStm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void addProduct(Product product){
+        try {
+            addProductStm.setInt(1, product.getId());
+            addProductStm.setString(2, product.getName());
+            addProductStm.setString(3, product.getPrice());
+            addProductStm.setInt(4, product.getAmount());
+            addProductStm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 
 }

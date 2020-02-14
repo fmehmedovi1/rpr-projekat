7eb0f8dc1dfe588f0501c9c83437d391df2ca7dc
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-14 16:42:31 +0100

    kreirana klasa ChangeInStorage sa atributima

---
+++ src/sample/ChangeInStorage.java
@@ -0,0 +1,8 @@
+package sample;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class ChangeInStorage {
+    private int id, product_id, storage_id;
+    private SimpleStringProperty typeOfChange;
+}

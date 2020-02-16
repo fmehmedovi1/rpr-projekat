4f3eb58ca13bff47792da11e58b55f318266c910
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-16 02:27:54 +0100

    izbrisani Property atributi, User je izvedena klasa Person

--- src/sample/User.java
+++ src/sample/User.java
@@ -1,86 +1,9 @@
 package sample;
 
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
-public class User {
+public class User extends Person{
     private int id;
-    private SimpleStringProperty firstName, lastName, username, email, password;
 
-    public User(int user_id, String firstName, String lastName, String username, String email, String password) {
-        this.id = user_id;
-        this.firstName = new SimpleStringProperty(firstName);
-        this.lastName = new SimpleStringProperty(lastName);
-        this.username = new SimpleStringProperty(username);
-        this.email = new SimpleStringProperty(email);
-        this.password = new SimpleStringProperty(password);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName.get();
-    }
-
-    public SimpleStringProperty firstNameProperty() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName.set(firstName);
-    }
-
-    public String getLastName() {
-        return lastName.get();
-    }
-
-    public SimpleStringProperty lastNameProperty() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName.set(lastName);
-    }
-
-    public String getUsername() {
-        return username.get();
-    }
-
-    public SimpleStringProperty usernameProperty() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username.set(username);
-    }
-
-    public String getEmail() {
-        return email.get();
-    }
-
-    public SimpleStringProperty emailProperty() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email.set(email);
-    }
-
-    public String getPassword() {
-        return password.get();
-    }
-
-    public SimpleStringProperty passwordProperty() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password.set(password);
-    }
+   
 }

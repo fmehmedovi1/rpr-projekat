f4044eae8b68d5352a3a5c96e1e190058e09f1af
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-02-16 02:28:28 +0100

    dodati konstruktor, get-eri i set-eri

--- src/sample/User.java
+++ src/sample/User.java
@@ -5,5 +5,20 @@ import javafx.beans.property.SimpleStringProperty;
 public class User extends Person{
     private int id;
 
-   
+    public User(int user_id, String firstName, String lastName, String username, String email, String password) {
+        this.id = user_id;
+        super.firstName = firstName;
+        super.lastName = lastName;
+        super.username = username;
+        super.email = email;
+        super.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }

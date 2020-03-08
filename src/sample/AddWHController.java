cbaf6fbb8b472d609eab311e3b9d9593875c0039
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-03-09 00:04:34 +0100

    dodata metoda updateProgressBar

--- src/sample/AddWHController.java
+++ src/sample/AddWHController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,7 +12,6 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -28,6 +28,7 @@ public class AddWHController {
 
     @FXML
     public void initialize() {
+        progressBar.setProgress(0.0);
         progressBar.setVisible(false);
     }
 
@@ -38,24 +39,28 @@ public class AddWHController {
             return;
         }
 
-        ResourceBundle resourceBundle = ResourceBundle.getBundle("Translation", currentLanguage);
-        userModel.getWarehouseDAO().addWarehouse(fldName.getText(), fldAddress.getText(), userModel.getCurrentUser().getId());
-
+//        progressBar.setVisible(true);
+//        Thread thread = new Thread(new bgThread());
+//        thread.start();
         progressBar.setVisible(true);
-        Thread thread = new Thread(new bgThread());
-        thread.start();
+        updateProgressBar();
 
-        HomepageController controller = new HomepageController(userModel, currentLanguage);
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"), resourceBundle);
-        loader.setController(controller);
-        Parent root = loader.load();
-        Stage stage = new Stage();
-        stage.setTitle("Home Page");
-        stage.setScene(new Scene(root, 550, 550));
-        stage.show();
+        if ((int) progressBar.getProgress() == 1) {
+            ResourceBundle resourceBundle = ResourceBundle.getBundle("Translation", currentLanguage);
+            userModel.getWarehouseDAO().addWarehouse(fldName.getText(), fldAddress.getText(), userModel.getCurrentUser().getId());
 
-        Stage stage2 = (Stage) fldName.getScene().getWindow();
-        stage2.close();
+            HomepageController controller = new HomepageController(userModel, currentLanguage);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"), resourceBundle);
+            loader.setController(controller);
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Home Page");
+            stage.setScene(new Scene(root, 550, 550));
+            stage.show();
+
+            Stage stage2 = (Stage) fldName.getScene().getWindow();
+            stage2.close();
+        }
     }
 
     private void warningAlert(String message) {
@@ -66,13 +71,15 @@ public class AddWHController {
         alert.showAndWait();
     }
 
-    class bgThread implements Runnable{
+    class bgThread implements Runnable {
+        public bgThread() {}
 
         @Override
         public void run() {
-            for(int i = 0; i < 100; i++){
+
+            for(int i = 1; i <= 100; i++){
                 try {
-                    progressBar.setProgress(i / 100.00);
+                    progressBar.setProgress((double)i / 100.0);
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -80,4 +87,23 @@ public class AddWHController {
             }
         }
     }
+
+    private void updateProgressBar() {
+        new Thread(() -> {
+
+            try {
+                for (int i = 0; i <= 100; i++) {
+                    progressBar.setProgress((double) i / 100.0);
+                    Thread.sleep(100);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+    }
+
+
+
 }
